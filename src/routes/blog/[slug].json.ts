42b9d467cd8f050e7c1076257d477527cf08ccb7
datoyacx <datoyacx@outlook.jp>
2021-09-20 04:15:31 +0700

    remove json method

--- src/routes/blog/[slug].json.ts
+++
@@ -1,6 +0,0 @@
-export async function get({ params }) {
-    let body = await fetch(`https://c0re.ba-ka.org/v1/content?code=${ params.slug }`).then( result => result.text() );
-    body = JSON.parse(body);
-    body = body['result']
-    return { body }
-}
--- src/routes/blog/index.json.ts
+++
@@ -1,6 +0,0 @@
-export async function get() {
-    let body = await fetch('https://c0re.ba-ka.org/v1/content?userid=3').then( result => result.text() );
-    body = JSON.parse(body);
-    body = body['result']
-    return { body }
-}
