bbb9844477d71f654fc9b898f243a47639352798
datoyacx <datoyacx@outlook.jp>
2021-09-21 05:23:04 +0700

    import style global

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,6 +6,10 @@
 	export const profile: string = 'https://avatars.githubusercontent.com/u/53964388';
 </script>
 
+<style lang="scss" global>
+	@import '../styles/global.scss';
+</style>
+
 <h1>toya</h1>
 <img src="{profile}" alt="toya profile">
 <p>👁️ 👃 👁️ pipo pipo</p>
