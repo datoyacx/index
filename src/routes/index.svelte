29dc19fe62f067dfc39bc1fb886f73ba16c99c45
datoyacx <datoyacx@outlook.jp>
2021-09-21 17:46:17 +0700

    move global scss from index to layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,13 +3,15 @@
 </svelte:head>
 
 <script lang="ts">
+	import Menu from '$lib/components/Menu.svelte';
+
 	export const profile: string = 'https://avatars.githubusercontent.com/u/53964388';
 </script>
 
-<style lang="scss" global>
-	@import '../styles/global.scss';
-</style>
-
-<h1>toya</h1>
-<img src="{profile}" alt="toya profile">
-<p>👁️ 👃 👁️ pipo pipo</p>
+<div class="page home">
+	<h1 class="title">toya</h1>
+	<p class="description">a lazy guy who want to sleep and write code in isekai</p>
+	<img src="{profile}" alt="toya profile">
+	<Menu/>
+	<p class="copyright">&copy; 2021</p>
+</div>
