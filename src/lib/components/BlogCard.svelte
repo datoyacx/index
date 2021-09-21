4665b954d61ca5237f10b0e40d91285dc5185217
datoyacx <datoyacx@outlook.jp>
2021-09-21 17:46:37 +0700

    add blog card components

---
+++ src/lib/components/BlogCard.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let code: string;
+    export let title: string;
+</script>
+
+<div class="blog-list">
+    <a href={`/blog/${code}`}>
+        <h2>{title}</h2>
+    </a>
+</div>
