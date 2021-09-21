fe421099b4b79d5d555623a39060a4ada8325dee
datoyacx <datoyacx@outlook.jp>
2021-09-21 17:45:09 +0700

    add more detail contact

--- src/routes/contact/index.svelte
+++ src/routes/contact/index.svelte
@@ -2,5 +2,21 @@
 	<title>contact</title>
 </svelte:head>
 
-<h1>contact</h1>
-<p>contact me <a href="https://cryw0rks.ba-ka.org/contact">here</a></p>
+<script lang="ts">
+	import Menu from '$lib/components/Menu.svelte';
+</script>
+
+<div class="page contact">
+	<div class="banner">
+		<img src="/image/contact.gif" alt="contact">
+	</div>
+	<Menu/>
+	<h1 class="title">contact</h1>
+	<ul>
+		<li><a href="https://d.ba-ka.org">discord</a></li>
+		<li><a href="mailto:datoyacx@outlook.jp">e-mail</a></li>
+		<li><a href="https://ah0.ba-ka.org/user/datoyacx">ah0 project</a></li>
+		<li><a href="https://github.com/datoyacx">github</a></li>
+	</ul>
+	<p class="copyright">&copy; 2021</p>
+</div>
