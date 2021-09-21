824d4016aa8087b31331d37ef4ba41457c511b1f
datoyacx <datoyacx@outlook.jp>
2021-09-21 17:46:28 +0700

    add menu components

---
+++ src/lib/components/Menu.svelte
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="/">home</a></li>
+        <li><a href="/blog">blog</a></li>
+        <li><a href="/project">project</a></li>
+        <li><a href="/contact">contact</a></li>
+    </ul>
+</nav>
