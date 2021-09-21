81923f14d4ec9843d86f18ca07b2bbfa36d239d4
datoyacx <datoyacx@outlook.jp>
2021-09-21 17:44:55 +0700

    add project page

---
+++ src/routes/project/index.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<title>project</title>
+</svelte:head>
+
+<script lang="ts">
+	import Menu from '$lib/components/Menu.svelte';
+</script>
+
+<div class="page project">
+	<div class="banner">
+		<img src="/image/project.gif" alt="project">
+	</div>
+	<Menu/>
+	<h1 class="title">project</h1>
+	<p>coming soon!</p>
+	<p class="copyright">&copy; 2021</p>
+</div>
