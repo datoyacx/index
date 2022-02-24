895d0812f59f6a0590cdf0da28418c3febf40871
datoyacx <datoyacx@outlook.jp>
2022-02-24 18:55:45 +0700

    add my project

--- src/routes/project/index.svelte
+++ src/routes/project/index.svelte
@@ -13,7 +13,48 @@
 	<Menu/>
 	<div class="content">
 		<h1 class="title">project</h1>
-		<p>coming soon!</p>
+		<p>yo this is project i ever worked, if you are interested with my work feel free to contact me :)</p>
+		<h3 class="portfolio-title">game</h3>
+		<ul class="portfolio-list">
+			<li>
+				<a href="https://www.roblox.com/games/5253868577/GHOST-ID-Project">ghost id project</a>
+				<ul>
+					<li>using roblox studio</li>
+					<li>scripting</li>
+					<li>animator</li>
+					<li>ui/ux</li>
+					<li>animation (simple)</li>
+				</ul>
+			</li>
+		</ul>
+		<h3 class="portfolio-title">website</h3>
+		<ul class="portfolio-list">
+			<li>
+				<a href="https://empugaris.com/">empugaris.com</a>
+				<ul>
+					<li>web slicing</li>
+				</ul>
+			</li>
+			<li>
+				<a href="https://sehatcruises.com/">sehatcruises.com</a>
+				<ul>
+					<li>web slicing</li>
+				</ul>
+			</li>
+			<li>
+				<a href="https://duplen.id/">duplen.id</a>
+				<ul>
+					<li>web slicing</li>
+					<li>web design</li>
+				</ul>
+			</li>
+			<li>
+				<a href="https://jaktel.com/">jaktel.com</a>
+				<ul>
+					<li>web slicing</li>
+				</ul>
+			</li>
+		</ul>
 	</div>
 	<p class="copyright">&copy; 2021</p>
 </div>
